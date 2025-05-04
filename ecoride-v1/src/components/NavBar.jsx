b3c7b661ba8f2b8686367bf55e880a65b47024e4
julesfremiot <fremiot.jules88@gmail.com>
2025-05-04 09:45:51 +0930

    Responsive Menu

--- ecoride-v1/src/components/NavBar.jsx
+++ ecoride-v1/src/components/NavBar.jsx
@@ -1,26 +1,36 @@
+import { useState } from "react"
+
 export default function NavBar(){
 
+    const [mobileMenu, openMenu] = useState(true);
+
     return (
-        <nav className="h-20 w-screen fixed top-0 shadow-lg flex items-center justify-between px-20">
-            <div className="menu-wrapper">
-                <div className="logo inline mr-20">Logo</div>
+        <nav className="h-20 w-screen fixed top-0 shadow-lg flex items-center justify-between lg:px-20 md:px-5">
+            <div className="flex px-10 w-screen items-center justify-between md:hidden">
+                <div>
+                <span>Logo</span>
+                </div>
+                <i onClick={() => {openMenu(prevState => !prevState)}} className={mobileMenu ? "bi bi-list text-skyBlue font-bold text-xl rounded px-1 block" : "bi bi-x text-skyBlue font-bold text-xl rounded px-1 block"}></i>
+            </div>
+            <div className="menu-wrapper hidden md:block">
+                <div className="logo inline lg:mr-20 md:mr-5">Logo</div>
                 <ul className="inline">
                 <li className="inline-block mr-7 p-2 text-skyBlue hover:text-oceanBlue font-semibold">Acceuil</li>
                     <li className="inline-block mr-7 p-2 text-skyBlue hover:text-oceanBlue font-semibold">Covoiturages</li>
                     <li className="inline-block mr-7 p-2 text-skyBlue hover:text-oceanBlue font-semibold">Contact</li>
                 </ul>
             </div>
-            <div className="action-wrapper">
-                <span className="text-skyBlue font-semibold hover:text-oceanBlue mr-10">
-                <i className="text-2xl bi bi-search text-skyBlue mr-4"></i>
+            <div className="action-wrapper hidden md:flex items-center justify-evenly">
+                <span className="text-skyBlue font-semibold hover:text-oceanBlue md:mr-3 lg:mr-10">
+                <i className="text-2xl bi bi-search text-skyBlue md:mr-2 lg:mr-4"></i>
                 Rechercher
                 </span>
-                <span className="text-skyBlue font-semibold hover:text-oceanBlue mr-10">
-                <i className="text-2xl bi bi-plus-circle-fill text-skyBlue mr-4"></i>
+                <span className="text-skyBlue font-semibold hover:text-oceanBlue md:mr-3 lg:mr-10">
+                <i className="text-2xl bi bi-plus-circle-fill text-skyBlue md: mr-2 lg:mr-4"></i>
                 Ajouter un trajet
                 </span>
-                <span className="text-skyBlue font-semibold hover:text-oceanBlue">
-                <i className="text-4xl bi bi-person-fill text-skyBlue mr-4"></i>
+                <span className="text-skyBlue xs:block font-semibold hover:text-oceanBlue">
+                <i className="text-4xl bi bi-person-fill text-skyBlue md:mr-2 lg:mr-4"></i>
                 </span>
             </div>
         </nav>
