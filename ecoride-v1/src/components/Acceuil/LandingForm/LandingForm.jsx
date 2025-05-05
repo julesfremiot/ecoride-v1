394d54d2a65b00b9f0c7b0f8429d831f0d4aa7c1
julesfremiot <fremiot.jules88@gmail.com>
2025-05-05 11:29:32 +0930

    changing to mobile app feeling

--- ecoride-v1/src/components/Acceuil/LandingForm/LandingForm.jsx
+++ ecoride-v1/src/components/Acceuil/LandingForm/LandingForm.jsx
@@ -1,17 +1,11 @@
 import "../../../components/componentscss/landingform.css"
+import landingIcon from '../../assets/landingpic.svg';
 export default function LandingForm(){
     return (
-        <div className=" bg-skyBlue flex flex-col align-center p-5 mt-20 form-container w-100 rounded-b">
-            <div className="bg-skyBlue w-100 rounded-t-md text-center text-white font-medium p-2">Book ton Ecoride!</div>
-            <form className="w-100 bg-white align-center flex flex-col justify-center rounded-b-md p-3 shadow-md">
-                <input type="text" placeholder="Ville de départ" id="ville-départ" className="p-2 border-b-2  outline-skyBlue"></input>
-                <input type="text" placeholder="Ville d'arrivée" id="ville-arrivée" className="p-2 border-b-2 outline-skyBlue mt-2"></input>
-                <label htmlFor="date-départ" className="text-gray-400 mt-4 ml-1 text-xs">Date de départ</label>
-                <input type="date" className="p-2 border-b-2 outline-skyBlue mt-2 text-gray-400" id="date-départ"></input>
-                <button type="submit" className="p-2 bg-skyBlue rounded text-white mt-5 hover:bg-indigo-800">
-                <i className="text-center text-lg bi bi-search mr-3 text-white"></i>
-                    Rechercher</button>
-            </form>
+        <div className="bg-orange-400 rounded-b-2xl mt-20 flex flex-col items-center py-7">
+        <img src={landingIcon} alt="Landing icon" className="w-2/4 h-2/4 block" />
+        <p className="font-semibold text-white text-2xl mt-7">Trouve ton Ecoride!</p>
+        <p className="font-medium text-white text-lg text-center mt-4 mb-2">Inscrit-toi et débloque<br></br> 1000+ trajets éco tout les jours.</p>
         </div>
     )
 }
