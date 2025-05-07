72ff7c171918588b5c045729f0c365971b6be483
julesfremiot <fremiot.jules88@gmail.com>
2025-05-07 11:36:58 +0930

    comitting changes to voyages section

---
+++ ecoride-v1/src/components/Acceuil/Role.jsx
@@ -0,0 +1,12 @@
+import passengerIcon from "../assets/passenger.svg"
+import driverIcon from "../assets/driver.svg"
+
+export default function Roles (){
+    return (
+        <>
+            <div>
+
+            </div>
+        </>
+    )
+}
--- ecoride-v1/src/components/Acceuil/Voyages.jsx
+++ ecoride-v1/src/components/Acceuil/Voyages.jsx
@@ -4,7 +4,7 @@ export default function Voyages(){
 
     return (
         <>
-            <div className="w-screen pt-10 bg-green-950 z-0 flex flex-col items-center pb-8 rounded-b-2xl shadow-md">
+            <div className="w-screen pt-10 bg-green-950 z-0 flex flex-col gap-4 items-center pb-8 rounded-b-2xl shadow-md">
                 <h3 className="text-4xl font-extrabold p-4 text-green-400 mb-4 mt-4 text-center">Des milliers de voyages tout les jours.</h3>
                 <p className="text-gray-100 text-center font-sm">Où vas-tu aujourd'hui?</p>
                 <div className="bg-white flex rounded w-10/12 py-4 shadow-md mt-6">
@@ -17,7 +17,7 @@ export default function Voyages(){
                     </div>
                     <div className="flex flex-col justify-between items-start ml-10 w-20">
                         <p className="leading-none text-gray-500">À partir de:</p>
-                        <p className="hover:translate-x-2 transform transition duration-300 ease-in-out text-2xl leading-none font-bold text-green-500 h-6 hover:text-green-700">8€<i class="bi bi-arrow-right text-xl ml-4"></i></p>
+                        <p className="hover:translate-x-2 transform transition duration-300 ease-in-out text-2xl leading-none font-bold text-green-500 h-6 hover:text-green-700">8€<i className="bi bi-arrow-right text-xl ml-4"></i></p>
                     </div>
                 </div>
                 <div className="bg-white flex rounded w-10/12 py-4 shadow-md mt-6">
@@ -30,7 +30,7 @@ export default function Voyages(){
                     </div>
                     <div className="flex flex-col justify-between items-start ml-10 w-20">
                         <p className="leading-none text-gray-500">À partir de:</p>
-                        <p className="hover:translate-x-2 transform transition duration-300 ease-in-out text-2xl leading-none font-bold text-green-500 h-6 hover:text-green-700">12€<i class="bi bi-arrow-right text-xl ml-4"></i></p>
+                        <p className="hover:translate-x-2 transform transition duration-300 ease-in-out text-2xl leading-none font-bold text-green-500 h-6 hover:text-green-700">12€<i className="bi bi-arrow-right text-xl ml-4"></i></p>
                     </div>
                 </div>
                 <div className="bg-white flex rounded w-10/12 py-4 shadow-md mt-6">
@@ -43,7 +43,7 @@ export default function Voyages(){
                     </div>
                     <div className="flex flex-col justify-between items-start ml-10 w-20">
                         <p className="leading-none text-gray-500">À partir de:</p>
-                        <p className="hover:translate-x-2 transform transition duration-300 ease-in-out text-2xl leading-none font-bold text-green-500 h-6 hover:text-green-700">16€<i class="bi bi-arrow-right text-xl ml-4"></i></p>
+                        <p className="hover:translate-x-2 transform transition duration-300 ease-in-out text-2xl leading-none font-bold text-green-500 h-6 hover:text-green-700">16€<i className="bi bi-arrow-right text-xl ml-4"></i></p>
                     </div>
                 </div>
                 <div className="bg-white flex rounded w-10/12 py-4 shadow-md mt-6">
@@ -56,10 +56,10 @@ export default function Voyages(){
                     </div>
                     <div className="flex flex-col justify-between items-start ml-10 w-20">
                         <p className="leading-none text-gray-500">À partir de:</p>
-                        <p className="hover:translate-x-2 transform transition duration-300 ease-in-out text-2xl leading-none font-bold text-green-500 h-6 hover:text-green-700">10€<i class="bi bi-arrow-right text-xl ml-4"></i></p>
+                        <p className="hover:translate-x-2 transform transition duration-300 ease-in-out text-2xl leading-none font-bold text-green-500 h-6 hover:text-green-700">10€<i className="bi bi-arrow-right text-xl ml-4"></i></p>
                     </div>
                 </div>
-                <button type="button" className="bg-orange-500 hover:bg-orange-700 font-semibold shadow-md rounded text-white p-3 mt-6">Découvre nos itinéraires populaires</button>
+                <button type="button" className="text-sm bg-orange-600 hover:bg-orange-800 font-semibold shadow-md rounded text-white p-3 mt-6">Découvre nos itinéraires populaires</button>
             </div>
         </>
     )
